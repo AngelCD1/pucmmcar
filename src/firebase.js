8785b7641bcc979f6c019c8bd4e5e096a065c8a1
AngelCD1 <angelcd658@gmail.com>
2026-04-07 15:03:53 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Configuración de Firebase con llaves directas para que funcione en Vercel/Internet
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDNX-GQEhUuM9-kPtOqe02ud2x-3HFA8-U",
+    authDomain: "pucmm-50e5c.firebaseapp.com",
+    projectId: "pucmm-50e5c",
+    storageBucket: "pucmm-50e5c.appspot.com",
+    messagingSenderId: "165753247439",
+    appId: "1:165753247439:web:58a44bc6e22a22e09209ba",
 };
 
 const app = initializeApp(firebaseConfig);
